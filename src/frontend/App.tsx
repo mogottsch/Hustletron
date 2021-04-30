69a18ad2f7256b73cec52ea41f47b43d8fcccab2
Moritz <mgottsc5@smail.uni-koeln.de>
2021-04-30 11:13:54 +0200

    Add images

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -6,6 +6,7 @@ import { makeStyles, styled } from '@material-ui/core/styles';
 import FormContainer from './generator/FormContainer';
 import Info from './generator/Info';
 import Updater from './Updater';
+import hustletronbot from '../../assets/hustletronbot.png';
 
 const WhiteBox = styled(Box)({
   background: 'white',
@@ -18,12 +19,20 @@ const useStyles = makeStyles({
     display: 'flex',
     alignItems: 'center',
   },
+  hustletronbot: {
+    position: 'fixed',
+    bottom: '0px',
+    right: '0px',
+    width: 'min(50vw, 50vh)',
+    zIndex: -1,
+  },
 });
 
 const Generator = () => {
   const classes = useStyles();
   return (
     <Container className={classes.root} maxWidth="sm">
+      <img className={classes.hustletronbot} alt="icon" src={hustletronbot} />
       <WhiteBox p={2}>
         <Grid container spacing={2}>
           <Grid item xs={12}>
